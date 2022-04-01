effab0089272ae654909cfcdc7e1a3e946330b57
João Pedro <jpedrodaibello1999@outlook.com>
2022-04-01 09:56:56 -0300

    fix: bugfix in shared_preferences encapsulation

--- lib/app/core/local_storage/shared_preferences_local_storage.dart
+++ lib/app/core/local_storage/shared_preferences_local_storage.dart
@@ -20,15 +20,15 @@ class SharedPreferencesLocalStorage implements LocalStorage {
   Future<P?> read<P>(String key) async {
     final sp = await _instance;
 
-    if (P is String) {
+    if (P == String) {
       return sp.getString(key) as P?;
-    } else if (P is int) {
+    } else if (P == int) {
       return sp.getInt(key) as P?;
-    } else if (P is bool) {
+    } else if (P == bool) {
       return sp.getBool(key) as P?;
-    } else if (P is double) {
+    } else if (P == double) {
       return sp.getDouble(key) as P?;
-    } else if (P is List) {
+    } else if (P == List) {
       return sp.getStringList(key) as P?;
     } else {
       throw Exception('Type not supported');
@@ -47,13 +47,13 @@ class SharedPreferencesLocalStorage implements LocalStorage {
 
     if (P is String) {
       sp.setString(key, value as String);
-    } else if (P is int) {
+    } else if (P == int) {
       sp.setInt(key, value as int);
-    } else if (P is bool) {
+    } else if (P == bool) {
       sp.setBool(key, value as bool);
-    } else if (P is double) {
+    } else if (P == double) {
       sp.setDouble(key, value as double);
-    } else if (P is List) {
+    } else if (P == List) {
       sp.setStringList(key, value as List<String>);
     } else {
       throw Exception('Type not supported');
